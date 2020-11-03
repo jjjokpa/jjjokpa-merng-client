71b3ddf4381fc2b33b59849825992845570aecc1
김동석 <jjjokpa13@gmail.com>
2020-11-03 18:04:55 +0900

    change base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -10,7 +10,7 @@ import
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://shrouded-castle-48611.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
